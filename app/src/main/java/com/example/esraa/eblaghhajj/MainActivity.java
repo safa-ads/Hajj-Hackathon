c6012bc01ee76a661f2f8177288031665a8b6842
EsraaaSalah <esraasalah171995@gmail.com>
2018-08-02 02:01:37 +0200

    First commit

---
+++ app/src/main/java/com/example/esraa/eblaghhajj/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.esraa.eblaghhajj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
