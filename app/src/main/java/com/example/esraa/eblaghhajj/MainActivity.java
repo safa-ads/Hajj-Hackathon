80c25c1359ab68f46f9df44c0b2817ec842a8a23
EsraaaSalah <esraasalah171995@gmail.com>
2018-08-02 04:13:36 +0200

    QR Code implemented

--- app/src/main/java/com/example/esraa/eblaghhajj/MainActivity.java
+++ app/src/main/java/com/example/esraa/eblaghhajj/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.esraa.eblaghhajj;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageButton buttonAmbulance;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonAmbulance = (ImageButton) findViewById(R.id.buttonAmbulance);
+        buttonAmbulance.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, QRReaderActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
